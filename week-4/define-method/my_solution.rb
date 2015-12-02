6f638aa02d7ccefe34412505a9b6212912352c49
JovankaN <jovankanikolovski@gmail.com>
2015-12-02 10:54:52 -0600

    DEfining methods

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(study,work,clean)
+  puts study
+  puts work
+  puts clean
+end
